43652cd1db2582d0c4765ce952b6cd8c7256aa23
Shiva942 <30700067+Shiva942@users.noreply.github.com>
2019-06-13 14:08:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,102 @@
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TouchableOpacity,
+  Dimensions
+} from "react-native";
+import {
+  createAppContainer,
+  createStackNavigator,
+  createBottomTabNavigator
+} from "react-navigation";
+//import HomeTab from "./app/screens/homeScreen";
+import LibraryTab from "./app/screens/libraryScreen";
+import BrowseTab from "./app/screens/browseScreen";
+import SearchTab from "./app/screens/searchScreen";
+import HomeTab from "./app/screens/mainScreen";
+import RadioTab from "./app/screens/radioScreen";
+import Test from "./app/screens/testScreen";
+//import { Icon } from "native-base";
+import { fadeIn } from "react-navigation-transitions";
+const SCREEN_WIDTH = Dimensions.get("window").width;
+const SCREEN_HEIGHT = Dimensions.get("window").height;
+
+const instructions = Platform.select({
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  android:
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
+});
+
+// const FadeTransition = (index, position) => {
+//   const scene = ;
+//    const outputOpacity=[0,1];
+//    const transition = position.interpolate({inputRange: sceneRange, outputRange: outputOpacity});
+//    return(){ opacity: transition }
+//
+//
+//
+// }
+//    const NavigationConfig = ()=>{
+//
+//    return{
+//      screenInterpolator{sceneProps}
+//
+//      }
+//
+//    }
+
+const AppNavigator = createBottomTabNavigator(
+  {
+    Home: {
+      screen: HomeTab
+    },
+    Browse: {
+      screen: BrowseTab
+    },
+    Search: {
+      screen: SearchTab
+    },
+    Radio: {
+      screen: RadioTab
+    },
+    Library: {
+      screen: LibraryTab
+    }
+  },
+  {
+    initialRouteName: "Home",
+    transitionConfig: () => fadeIn(3000),
+    tabBarOptions: {
+      activeTintColor: "white",
+      labelStyle: {
+        fontSize: 10
+      },
+      style: {
+        backgroundColor: "#1A1A1A",
+        height: SCREEN_HEIGHT * 0.09
+      }
+    }
+  }
+);
+
+const FinalNavigator = createAppContainer(AppNavigator);
+
+export default class App extends Component {
+  async componentDidMount() {
+    if (Platform.OS === "android" || Platform.OS === "ios") {
+      StatusBar.setHidden(false);
+      StatusBar.setBackgroundColor("#1A1A1A", false);
+      StatusBar.setBarStyle("dark-content", true);
+    } else {
+      StatusBar.setHidden(true);
+    }
+  }
+  render() {
+    return <FinalNavigator />;
+  }
+}
